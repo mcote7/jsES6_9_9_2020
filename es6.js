d0fad2dbdd6631fbe6f2fb59ecddc52302b4bae1
Michael Cote <michaelcote7@outlook.com>
2020-09-09 22:02:39 -0700

    .

--- es6.js
+++ es6.js
@@ -1,9 +1,28 @@
 
-let largestString;
+// let largestString;
 
-const findLargestString = (strings) => {
-    return largestString =  strings.reduce((a,b)=> a.length > b.length ? a : b, '');
-    // set largestString to point to the 
-    // longest string found in the strings array passed in
-}
-console.log("result", findLargestString(["the", "quick", "brown", "fox", "jumped", "over", "some", "shit"]));
+// const findLargestString = (strings) => {
+//     return largestString =  strings.reduce((a,b)=> a.length > b.length ? a : b, '');
+//     // set largestString to point to the 
+//     // longest string found in the strings array passed in
+// }
+// console.log("result", findLargestString(["the", "quick", "brown", "fox", "jumped", "over", "some", "shit"]));
+
+// const array = [2,7,6,8,4];
+// const add = (a,b) => a + b;
+// console.log(add(...array));
+
+// const names = [
+//   'bob', 
+//   ...[
+//       'donald'
+//   ],
+//   'suzy', 
+//   'lacy',
+//   ...[
+//       'richard', 
+//       'alex'
+//   ]
+//  ]
+//  console.log(names);
+
